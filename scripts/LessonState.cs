4764b176ed4f67fb7e93f03e6dc4094a60ccc5ed
approachcircle <osc-mcd@protonmail.com>
2025-06-13 17:06:21 +0100

    reference embedded scene files instead of filesystem specific files

--- scripts/LessonState.cs
+++ scripts/LessonState.cs
@@ -2,18 +2,18 @@
 
 namespace JudgementDay;
 
-public class LessonState
+public static class LessonState
 {
     public static int CurrentLesson { get; set; } = 0;
-    public static int LessonCount { get; set; } = 0;
+    public static int LessonCount { get; private set; } = 0;
 
     public static void SetLessonCount()
     {
-        LessonState.CurrentLesson = 1;
-        LessonState.LessonCount = 0;
-        while (FileAccess.FileExists($"res://scenes/lessons/Lesson{LessonState.LessonCount + 1}.tscn"))
+        CurrentLesson = 1;
+        LessonCount = 0;
+        while (ResourceLoader.Exists($"res://scenes/lessons/Lesson{LessonState.LessonCount + 1}.tscn"))
         {
-            LessonState.LessonCount++;
+            LessonCount++;
         }
     }
 }
